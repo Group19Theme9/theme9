9b1af0b957b9293e56ee7d2360ae4abbf4446327
Amanda <amhu0002@stud.kea.dk>
2024-11-19 11:08:36 +0100

    java

---
+++ src/js/java.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", onScroll);
+
+function onScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
